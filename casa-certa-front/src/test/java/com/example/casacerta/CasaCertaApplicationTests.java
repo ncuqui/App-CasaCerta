82fb556a9d1a9e22f9e3b42bec7bf6331ccf6c24
Davi Batista <dev.davibatista@gmail.com>
2026-05-16 13:24:50 -0300

    Push v1.0Back-End / Front-End

---
+++ casa-certa-front/src/test/java/com/example/casacerta/CasaCertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casacerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaCertaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
